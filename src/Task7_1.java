469b38579eab4ae4e9bc658a3016befde717cddc
schlosserleo <gymnasium.ottweiler@gmail.com>
2023-11-30 15:56:09 +0100

    commit

--- src/Task7_1.java
+++ src/Task7_1.java
@@ -1,11 +1,11 @@
 public class Task7_1 {
     public static void main(String[] args) {
         char[] sequence = {'G', 'A', 'T', 'T', 'A', 'C', 'A'};
-        int numPur = countPurines(sequence);
-        int numPyr = countPyrimidines(sequence);
-        System.out.println(numPyr + " " + numPur);
+        char[] revcomp = reverseComplementSequence(sequence);
+        for (char x: revcomp){
+            System.out.println(x);
+        }
     }
-
     public static int countBases(char[] sequence, char toFind) {
         int count = 0;
         for (char x : sequence) {
@@ -13,7 +13,6 @@ public class Task7_1 {
         }
         return count;
     }
-
     public static int[] countBases(char[] sequence) {
         int[] result = new int[4];
         for (char x : sequence) {
@@ -27,15 +26,38 @@ public class Task7_1 {
                 result[3]++;
             }
         }
+        return result;
     }
-
     public static int countPurines(char[] sequence) {
         return countBases(sequence, 'A') + countBases(sequence, 'G');
     }
-
     public static int countPyrimidines(char[] sequence) {
         return countBases(sequence, 'T') + countBases(sequence, 'C');
     }
-
-
+    public static char[] complementSequence(char[] sequence){
+        char[] result = new char [sequence.length];
+        for (int i = 0; i < sequence.length; i++){
+            if (sequence [i] == 'A'){
+                result[i] = 'T';
+            } else if (sequence[i] == 'T') {
+                result[i] = 'A';
+            } else if (sequence[i] == 'G') {
+                result[i] = 'C';
+            } else if (sequence[i] == 'C') {
+                result[i] = 'G';
+            }
+        }
+        return result;
+    }
+    public static char[] reverseSequence(char[] sequence){
+        char[] result = new char[sequence.length];
+        for (int i = sequence.length; i >= 1; i--){
+            result[sequence.length - i] = sequence[i - 1];
+        }
+        return result;
+    }
+    public static char[] reverseComplementSequence(char[] sequence)
+    {
+        return reverseSequence(complementSequence(sequence));
+    }
 }
