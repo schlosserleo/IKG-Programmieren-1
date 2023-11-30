d29183782270735e0e16d1888887259f69fc7cce
schlosserleo <gymnasium.ottweiler@gmail.com>
2023-11-30 13:56:56 +0100

    commit

---
+++ src/ArtObject.java
@@ -0,0 +1,8 @@
+public class ArtObject {
+    public char Status;
+    public double Price;
+    public ArtObject(char status, double price){
+        Status = status;
+        Price = price;
+    }
+}
--- src/Aufgabe7Kultur.java
+++ src/Aufgabe7Kultur.java
@@ -1,2 +1,21 @@
 public class Aufgabe7Kultur {
+    public static void main(String[] args){
+
+    }
+    public static double[] priceMetrics(ArtObject[] objects){
+        double smallest, biggest;
+        smallest = biggest = objects[0].Price;
+        double avg, sum;
+        sum = 0;
+        for (ArtObject x : objects) {
+            if (x.Price <= smallest) {
+                smallest = x.Price;
+            } else if (x.Price >= biggest) {
+                biggest = x.Price;
+            }
+            sum += x;
+        }
+        avg = sum / objects.length;
+        return new double[]{smallest, biggest, avg};
+    }
 }
